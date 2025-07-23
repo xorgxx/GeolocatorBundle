aca8b6a18bc32a6d0dea12a14d390af66f71ddcf
Xorg <xorg@neonatis.com>
2025-07-23 11:10:48 +0200

    refactory ...

--- src/GeolocatorBundle.php
+++ src/GeolocatorBundle.php
@@ -22,4 +22,11 @@ class GeolocatorBundle extends Bundle
     {
         return \dirname(__DIR__);
     }
+
+    public function configureRoutes(RoutingConfigurator $routes): void
+    {
+        // Charger automatiquement les routes depuis les contrôleurs
+        $routes->import('../Controller/', 'attribute');
+    }
+
 }
